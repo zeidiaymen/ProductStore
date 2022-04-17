d48e349c31013786a6d110f47bfa07b7245701f6
zeidiaymen <73137612+zeidiaymen@users.noreply.github.com>
2022-04-17 14:20:31 +0100

    Add project files.

---
+++ PS.Data/Infrastructures/DataBaseFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PS.Data.Infrastructures
+{
+    public class DataBaseFactory : Disposable, IDataBaseFactory
+    {
+        public PSContext DataContext => new PSContext();
+
+
+        public override void DisposeCore()
+        {
+            DataContext.Dispose();
+        }
+    }
+}
